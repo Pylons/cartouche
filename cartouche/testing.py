5938c1c00116bf3df2fde10acb39c4b3e20aa8ed
Tres Seaver <tseaver@agendaless.com>
2010-12-23 14:13:33 -0500

    Add separate testing components.

---
+++ cartouche/testing.py
@@ -0,0 +1,41 @@
+# Testing app / config
+from repoze.sendmail.interfaces import IMailDelivery
+from zope.interface import implements
+from cartouche.interfaces import IPendingRegistrations
+
+DIVIDER =  "#" * 80
+
+class FauxMailDelivery(object):
+    implements(IMailDelivery)
+
+    def send(self, from_addr, to_addrs, message):
+        print DIVIDER
+        print 'From:    %s' % from_addr
+        print 'To:      %s' % ', '.join(to_addrs)
+        print '-' * 80
+        print message
+        print DIVIDER
+
+
+class Dummy(object):
+    def __init__(self, **kw):
+        self.__dict__.update(kw)
+
+
+class FauxPendingRegistrations(object):
+    implements(IPendingRegistrations)
+    _pending_store = {} # yes, a mutable default
+
+    def __init__(self, context):
+        pass
+
+
+    def set(self, email, security_question, security_answer, token):
+        info = Dummy(email=email,
+                     security_question=security_question,
+                     security_answer=security_answer,
+                     token=token)
+        self._pending_store[email] = info
+
+    def get(self, email, default=None):
+        return self._pending_store.get(email, default)
