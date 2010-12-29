790a0732c9ec96eb4358c333620bce78e4e95cb1
Tres Seaver <tseaver@agendaless.com>
2010-12-29 18:09:02 -0500

    Add account recovery view.

--- cartouche/login.py
+++ cartouche/login.py
@@ -11,7 +11,9 @@
 # FITNESS FOR A PARTICULAR PURPOSE
 #
 ##############################################################################
+from email.message import Message
 
+from colander import Email
 from colander import Schema
 from colander import SchemaNode
 from colander import String
@@ -21,14 +23,19 @@ from deform import ValidationFailure
 from deform.widget import PasswordWidget
 from pyramid.renderers import get_renderer
 from pyramid.url import model_url
+from repoze.sendmail.interfaces import IMailDelivery
 from repoze.who.api import get_api
 from webob.exc import HTTPFound
 
+from cartouche.interfaces import IRegistrations
+from cartouche.persistence import ConfirmedRegistrations
+from cartouche._util import _delivery
 from cartouche._util import _view_url
 
 class Login(Schema):
     login_name = SchemaNode(String())
     password = SchemaNode(String(), widget=PasswordWidget())
+
 
 def login_view(context, request):
     form = Form(Login(), buttons=('login',))
@@ -47,6 +54,7 @@ def login_view(context, request):
             api = get_api(request.environ)
             identity, headers =  api.login(credentials)
             if identity is not None:
+                # TODO: came_from handling
                 return HTTPFound(location=model_url(context, request),
                                  headers=headers)
             message = 'Login failed'
@@ -63,7 +71,66 @@ def login_view(context, request):
                                              'reset_password.html'),
            }
 
+
 def logout_view(context, request):
     api = get_api(request.environ)
     headers =  api.logout()
+    # TODO: make after-logout URL configurable?
     return HTTPFound(location=model_url(context, request), headers=headers)
+
+
+RECOVERY_EMAIL = """
+Your login name on this site is:
+
+  %(login)s
+
+If you do not still have the login page open, you can visit it via
+this URL:
+
+  %(login_url)s
+
+"""
+
+
+class RecoverAccount(Schema):
+    email = SchemaNode(String(), validator=Email())
+
+
+def recover_account_view(context, request):
+    form = Form(RecoverAccount(), buttons=('recover',))
+    rendered_form = form.render(null)
+    confirmed = request.registry.queryAdapter(context, IRegistrations,
+                                              name='confirmed')
+    if confirmed is None:  #pragma NO COVERAGE
+        confirmed = ConfirmedRegistrations(context)
+    login_url = _view_url(context, request, 'login_url', 'login.html')
+    registry = request.registry
+
+    if 'recover' in request.POST:
+        try:
+            appstruct = form.validate(request.POST.items())
+        except ValidationFailure, e: #pragma NO COVER
+            rendered_form = e.render()
+        else:
+            email = appstruct['email']
+            record = confirmed.get_by_email(email)
+            if record is not None:
+                from_addr = registry.settings['cartouche.from_addr']
+                login = record.login
+                body = RECOVERY_EMAIL % {'login': login,
+                                         'login_url': login_url}
+                delivery = registry.queryUtility(IMailDelivery,
+                                                 default=_delivery)
+                message = Message()
+                message.set_payload(body)
+                delivery.send(from_addr, [email], message)
+            #else: # XXX not reporting lookup errors for now
+            return HTTPFound(location=login_url)
+
+    main_template = get_renderer('templates/main.pt')
+    return {'main_template': main_template.implementation(),
+            'rendered_form': rendered_form,
+            'reset_password_url': _view_url(context, request,
+                                             'reset_password_url',
+                                             'reset_password.html'),
+           }
--- cartouche/tests/test_login.py
+++ cartouche/tests/test_login.py
@@ -31,7 +31,47 @@ class _Base(object):
         from pyramid.testing import DummyRequest
         return DummyRequest(**kw)
 
-class LoginTests(_Base, unittest.TestCase):
+    def _registerConfirmed(self):
+        from cartouche.interfaces import IRegistrations
+        by_uuid = {}
+        by_login = {}
+        by_email = {}
+        class DummyConfirmed:
+            def __init__(self, context):
+                pass
+            def get(self, key, default=None):
+                return by_uuid.get(key, default)
+            def get_by_login(self, login, default=None):
+                uuid = by_login.get(login)
+                if uuid is None:
+                    return default
+                return by_uuid.get(uuid, default)
+            def get_by_email(self, email, default=None):
+                uuid = by_email.get(email)
+                if uuid is None:
+                    return default
+                return by_uuid.get(uuid, default)
+            def set(self, key, **kw):
+                old_record = by_uuid.get(key)
+                if old_record is not None:
+                    del by_login[old_record.login]
+                    del by_email[old_record.email]
+                record =  Dummy(**kw)
+                by_uuid[key] = record
+                by_login[record.login] = key
+                by_email[record.email] = key
+            def remove(self, key):
+                info = by_uuid[key]
+                del by_uuid[key]
+                del by_login[info.login]
+                del by_email[info.email]
+        self.config.registry.registerAdapter(DummyConfirmed,
+                                             (None,), IRegistrations,
+                                             name='confirmed')
+        return by_uuid, by_login, by_email
+
+
+class Test_login(_Base, unittest.TestCase):
 
     def _callFUT(self, context=None, request=None):
         from cartouche.login import login_view
@@ -144,7 +184,7 @@ class LoginTests(_Base, unittest.TestCase):
             self.failUnless(response.headers[key] is value)
 
 
-class LogoutTests(_Base, unittest.TestCase):
+class Test_logout(_Base, unittest.TestCase):
 
     def _callFUT(self, context=None, request=None):
         from cartouche.login import logout_view
@@ -168,6 +208,130 @@ class LogoutTests(_Base, unittest.TestCase):
             self.assertEqual(response.headers.get(key), value)
 
 
+class Test_recover_account(_Base, unittest.TestCase):
+
+    def _callFUT(self, context=None, request=None):
+        from cartouche.login import recover_account_view
+        if context is None:
+            context = self._makeContext()
+        if request is None:
+            request = self._makeRequest()
+        return recover_account_view(context, request)
+
+    def test_GET(self):
+        import re
+        INPUT = re.compile('<input.*name="(?P<name>\w+)"', re.MULTILINE)
+        mtr = self.config.testing_add_template('templates/main.pt')
+
+        info = self._callFUT()
+
+        main_template = info['main_template']
+        self.failUnless(main_template is mtr.implementation())
+        rendered_form = info['rendered_form']
+        inputs = [x for x in INPUT.findall(rendered_form)
+                        if not x.startswith('_')]
+        self.assertEqual(inputs, ['email'])
+        self.assertEqual(info['reset_password_url'],
+                         'http://example.com/reset_password.html')
+
+    def test_POST_w_errors(self):
+        import re
+        SUMMARY_ERROR = re.compile('<h3[^>]*>There was a problem', re.MULTILINE)
+        FIELD_ERROR = re.compile('<p class="error"', re.MULTILINE)
+        POST = {'email': '',
+                'recover': '',
+               }
+        mtr = self.config.testing_add_template('templates/main.pt')
+        context = self._makeContext()
+        request = self._makeRequest(POST=POST)
+
+        info = self._callFUT(context, request)
+
+        main_template = info['main_template']
+        self.failUnless(main_template is mtr.implementation())
+        rendered_form = info['rendered_form']
+        self.failUnless(SUMMARY_ERROR.search(rendered_form))
+        self.failUnless(FIELD_ERROR.search(rendered_form))
+
+    def test_POST_email_miss_still_redirects(self):
+        from repoze.sendmail.interfaces import IMailDelivery
+        from webob.exc import HTTPFound
+
+        FROM_EMAIL = 'admin@example.com'
+        TO_EMAIL = 'phred@example.com'
+        POST = {'email': TO_EMAIL,
+                'recover': '',
+               }
+        self.config.registry.settings['cartouche.from_addr'] = FROM_EMAIL
+        delivery = DummyMailer()
+        self.config.registry.registerUtility(delivery, IMailDelivery)
+        context = self._makeContext()
+        request = self._makeRequest(POST=POST)
+
+        response = self._callFUT(context, request)
+
+        self.failUnless(isinstance(response, HTTPFound))
+        login_url = 'http://example.com/login.html'
+        self.assertEqual(response.location, login_url)
+        self.assertEqual(delivery._sent, None)
+
+    def test_POST_email_hit_no_login_url_override(self):
+        from repoze.sendmail.interfaces import IMailDelivery
+        from webob.exc import HTTPFound
+
+        FROM_EMAIL = 'admin@example.com'
+        TO_EMAIL = 'phred@example.com'
+        POST = {'email': TO_EMAIL,
+                'recover': '',
+               }
+        self.config.registry.settings['cartouche.from_addr'] = FROM_EMAIL
+        delivery = DummyMailer()
+        self.config.registry.registerUtility(delivery, IMailDelivery)
+        by_uuid, by_login, by_email = self._registerConfirmed()
+        by_uuid['UUID'] = Dummy(login='login')
+        by_email[TO_EMAIL] = 'UUID'
+        context = self._makeContext()
+        request = self._makeRequest(POST=POST)
+
+        response = self._callFUT(context, request)
+
+        self.failUnless(isinstance(response, HTTPFound))
+        login_url = 'http://example.com/login.html'
+        self.assertEqual(response.location, login_url)
+        self.assertEqual(delivery._sent[0], FROM_EMAIL)
+        self.assertEqual(list(delivery._sent[1]), [TO_EMAIL])
+        self.failUnless(login_url in delivery._sent[2].get_payload())
+
+    def test_POST_email_hit_w_login_url_override(self):
+        from repoze.sendmail.interfaces import IMailDelivery
+        from webob.exc import HTTPFound
+
+        LOGIN = '/login_form.html'
+        FROM_EMAIL = 'admin@example.com'
+        TO_EMAIL = 'phred@example.com'
+        POST = {'email': TO_EMAIL,
+                'recover': '',
+               }
+        self.config.registry.settings['cartouche.from_addr'] = FROM_EMAIL
+        self.config.registry.settings['cartouche.login_url'] = LOGIN
+        delivery = DummyMailer()
+        self.config.registry.registerUtility(delivery, IMailDelivery)
+        by_uuid, by_login, by_email = self._registerConfirmed()
+        by_uuid['UUID'] = Dummy(login='login')
+        by_email[TO_EMAIL] = 'UUID'
+        context = self._makeContext()
+        request = self._makeRequest(POST=POST)
+
+        response = self._callFUT(context, request)
+
+        self.failUnless(isinstance(response, HTTPFound))
+        login_url = 'http://example.com%s' % LOGIN
+        self.assertEqual(response.location, login_url)
+        self.assertEqual(delivery._sent[0], FROM_EMAIL)
+        self.assertEqual(list(delivery._sent[1]), [TO_EMAIL])
+        self.failUnless(login_url in delivery._sent[2].get_payload())
+
+
 class FauxAPI(object):
     LOGIN_HEADERS = [('Faux-Cookie', 'gingersnap')]
     LOGOUT_HEADERS = [('Forget', 'Me')]
@@ -185,3 +349,15 @@ class FauxAPI(object):
     def logout(self, identifier_name=None):
         self._called_with = (identifier_name,)
         return self.LOGOUT_HEADERS
+
+
+class DummyMailer:
+    _sent = None
+
+    def send(self, from_addr, to_addrs, message):
+        self._sent = (from_addr, to_addrs, message)
+
+
+class Dummy:
+    def __init__(self, **kw):
+        self.__dict__.update(kw)
