5957f3e979e86026d734b2018e94929ac4f42ba5
Tres Seaver <tseaver@agendaless.com>
2010-12-30 17:44:12 -0500

    Add header.

--- cartouche/__init__.py
+++ cartouche/__init__.py
@@ -1,3 +1,17 @@
+##############################################################################
+#
+# Copyright (c) 2010 Agendaless Consulting and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the BSD-like license at
+# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
+# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
+# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+# FITNESS FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+
 from pyramid.configuration import Configurator
 from repoze.zodbconn.finder import PersistentApplicationFinder
 
