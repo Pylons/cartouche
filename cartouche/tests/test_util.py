9b7422041ecd7943c959c8db54c86b72df7b862b
Tres Seaver <tseaver@agendaless.com>
2010-12-30 14:06:59 -0500

    Add tests for 'view_url'.

--- cartouche/tests/test_util.py
+++ cartouche/tests/test_util.py
@@ -14,7 +14,7 @@
 import unittest
 
 
-class Test_getRandomToken(unittest.TestCase):
+class _Base(object):
 
     def setUp(self):
         from pyramid.configuration import Configurator
@@ -24,15 +24,62 @@ class Test_getRandomToken(unittest.TestCase):
     def tearDown(self):
         self.config.end()
 
+    def _makeContext(self, **kw):
+        from pyramid.testing import DummyModel
+        return DummyModel(**kw)
+
+    def _makeRequest(self, **kw):
+        from pyramid.testing import DummyRequest
+        return DummyRequest(**kw)
+
+
+class Test_view_url(_Base, unittest.TestCase):
+
+    def _callFUT(self, context=None, request=None,
+                 key='view_url', default_name='view.html', **extra_qs):
+        from cartouche.util import view_url
+        if context is None:
+            context = self._makeContext()
+        if request is None:
+            request = self._makeRequest()
+        return view_url(context, request, key, default_name, **extra_qs)
+
+    def test_wo_utility_wo_extra_query(self):
+        self.assertEqual(self._callFUT(), 'http://example.com/view.html')
+
+    def test_wo_utility_w_extra_query(self):
+        self.assertEqual(self._callFUT(foo='bar'),
+                         'http://example.com/view.html?foo=bar')
+
+    def test_w_utility_relative_wo_extra_query(self):
+        self.config.registry.settings['cartouche.view_url'] = '/somewhere.html'
+        self.assertEqual(self._callFUT(), 'http://example.com/somewhere.html')
+
+    def test_w_utility_absolute_wo_extra_query(self):
+        self.config.registry.settings['cartouche.view_url'
+                                     ] = 'http://other.example.com/'
+        self.assertEqual(self._callFUT(), 'http://other.example.com/')
+
+    def test_w_utility_relative_w_extra_query(self):
+        self.config.registry.settings['cartouche.view_url'
+                                     ] = '/somewhere.html?foo=bar'
+        self.assertEqual(self._callFUT(baz='qux'),
+                         'http://example.com/somewhere.html?foo=bar&baz=qux')
+
+    def test_w_utility_absolute_w_extra_query(self):
+        self.config.registry.settings['cartouche.view_url'
+                                     ] = 'http://other.example.com/?foo=bar'
+        self.assertEqual(self._callFUT(baz='qux'),
+                         'http://other.example.com/?foo=bar&baz=qux')
+
+
+class Test_getRandomToken(_Base, unittest.TestCase):
+
     def _callFUT(self, request=None):
         from cartouche.util import getRandomToken
         if request is None:
             request = self._makeRequest()
         return getRandomToken(request)
-
-    def _makeRequest(self, **kw):
-        from pyramid.testing import DummyRequest
-        return DummyRequest(**kw)
 
     def test_wo_utility(self):
         from uuid import UUID
